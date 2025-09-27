f99c835f1f4bd621790a35bb48bdcfcb60ed4335
Filip Gorny <filip.gorny@gmail.com>
2025-09-27 13:26:19 +0200

    Added clipboard unnamed set

--- lua/config/clipboard.lua
+++ lua/config/clipboard.lua
@@ -1,16 +1,5 @@
-vim.opt.clipboard = "unnamedplus"
+vim.api.nvim_set_option("clipboard", "unnamed")
 
--- mapowania dla bezpieczeństwa
-local opts = { noremap = true, silent = true }
-vim.keymap.set("v", "y", '"+y', opts)
-vim.keymap.set("n", "yy", '"+yy', opts)
-vim.keymap.set("v", "d", '"+d', opts)
-vim.keymap.set("n", "dd", '"+dd', opts)
-vim.keymap.set("n", "p", '"+p', opts)
-vim.keymap.set("n", "P", '"+P', opts)
-vim.keymap.set("v", "p", '"+p', opts)
-vim.keymap.set("v", "P", '"+P', opts)
-
-if vim.fn.has("clipboard") == 0 then
+vim.fn.has("clipboard") == 0 then
   vim.notify("Neovim nie ma wsparcia +clipboard!", vim.log.levels.WARN)
 end
